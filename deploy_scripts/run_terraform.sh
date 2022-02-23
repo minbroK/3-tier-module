28ac62df5db1274b5079912e47068704ea3475fc
minbroK <kmh6132@naver.com>
2022-02-23 07:20:38 +0000

    terraform-codepipeline-commit-3

--- deploy_scripts/run_terraform.sh
+++ deploy_scripts/run_terraform.sh
@@ -1,2 +1,3 @@
 terraform -chdir=/home/ec2-user/project/terraform/eworld-infra-module/stg/ init
 terraform -chdir=/home/ec2-user/project/terraform/eworld-infra-module/stg/ apply -auto-approve
+//terraform init apply를 실행할 곳을 지정
