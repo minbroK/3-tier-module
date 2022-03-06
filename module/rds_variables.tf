8045c1bee24dca30b5c60f4a468a0fc4f5c4ffe0
minbroK <86211743+minbroK@users.noreply.github.com>
2022-03-07 08:26:42 +0900

    Update rds_variables.tf

--- module/rds_variables.tf
+++ module/rds_variables.tf
@@ -1,4 +1,4 @@
-/*
+
 variable "rds_instance_type" {
   type = string
 }
@@ -15,19 +15,14 @@ variable "engine_version" {
   type = string
 }
 
-
 variable "allocated_storage" {
   type = string
 }
-*/
 
-/*
 variable "max_allocated_storage" {
   type = string
 }
-*/
 
-/*
 variable "multi_az" {
   type = bool
 }
@@ -36,8 +31,6 @@ variable "rds_username" {
   type = string
 }
 
-
 variable "rds_password" {
   type = string
 }
-*/
